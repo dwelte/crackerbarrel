a327cf863c007287d3eb4d2a92ab31a3377411a9
David Welte <dwelte@climate.com>
2016-05-27 13:38:21 -0700

    Let's get it started

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to crackerbarrel
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/crackerbarrel/core_test.clj
@@ -0,0 +1,7 @@
+(ns crackerbarrel.core-test
+  (:require [clojure.test :refer :all]
+            [crackerbarrel.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
